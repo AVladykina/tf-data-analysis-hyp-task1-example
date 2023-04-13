96c7caa228830280876a293911c2f898cb065bc5
AVladykina <55153864+AVladykina@users.noreply.github.com>
2023-04-12 23:03:33 -0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,16 +9,13 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
+    p1 = x_success / x_cnt
+    p2 = y_success / y_cnt
+    p = (x_success + y_success) / (x_cnt + y_cnt)
+    z = (p1 - p2) / (p * (1 - p) * (1 / x_cnt + 1 / y_cnt)) ** 0.5
+    p_value = 2 * (1 - norm.cdf(abs(z)))
   
     alpha=0.05
-    p_control = x_success / x_cnt
-    p_test = y_success / y_cnt
-    p_pool = (x_success + y_success) / (x_cnt + y_cnt)
-
-    se_diff = math.sqrt(p_pool * (1 - p_pool) * (1/x_cnt + 1/y_cnt))
-    z_score = (p_test - p_control) / se_diff
-
-    p_value = 2 * (1 - norm.cdf(abs(z_score)))
     return p_value < alpha
     # Измените код этой функции
     # Это будет вашим решением
